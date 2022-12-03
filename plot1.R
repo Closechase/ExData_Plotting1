cb9a1aff257bc2c5e2d80d4ee15074c6c77625ec
Aayush Asrey <aasrey_be20@thapar.edu>
2022-12-04 00:12:53 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,15 +1,22 @@
-data <- read.delim(file = "C:\\Users\\Aayush\\Desktop\\sem5\\datascience\\coursera assignments\\household_power_consumption.txt", header = TRUE, sep = ";", dec = ".")
+#library("swirl")
+#install_course("Exploratory_Data_Analysis")
+#swirl()
 
+#head(pollution)
+
+data <- read.delim(file = "C:\\Users\\Aayush\\Desktop\\sem5\\datascience\\coursera assignments\\household_power_consumption.txt", header = TRUE, sep = ";", dec = ".")
 data
 
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+class(data$Date)
 
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+class(data$Date)
+
+data
 
 data <- data[data$Date == as.Date("2007-02-01", format = "%Y-%m-%d") | data$Date == as.Date("2007-02-02", format = "%Y-%m-%d"), ]
 #data[data$Date == as.Date("2007-02-02", format = "%Y-%m-%d"),]
 #data
-strftime(data$Date, "%A")
-png(filename = "plot2.png",width = 480, height = 480, units = "px" )
-plot(data$Date, data$Global_active_power,type ="l")
-
+png(filename = "plot1.png",width = 480, height = 480, units = "px" )
+hist(as.numeric(data$Global_active_power), main = "Global Active Power",xlab = "Global Active Power (kilowatts)", ylab = "Frequency",col = "red")
 dev.off()
